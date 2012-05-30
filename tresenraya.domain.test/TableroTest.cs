f61963a860ede91da57215efbb5e90fe89df142b
Pablo C. Vergara Castro <pablocristobal@gmail.com>
2012-05-30 07:26:46 +0200

    Agrego los ficheros pasados por Adri

---
+++ tresenraya.domain.test/TableroTest.cs
@@ -0,0 +1,26 @@
+﻿
+
+using Xunit;
+namespace tresenraya.domain.test
+{
+    public class TableroTest
+    {
+        private string ficha="*";
+        private int columna=0;
+        private int fila=2;
+        [Fact]
+        public void cuando_añado_la_primera_ficha_en_la_primera_columna_esta_se_coloca_en_la_ultima_fila_de_esa_columna() 
+        {
+            var tablero = new Tablero();
+            
+            tablero.addFicha(ficha,columna);
+            Assert.Equal(ficha,tablero.getCelda(fila,columna));
+        }
+        [Fact]
+        public void cuando_añado_una_segunda_ficha_en_la_segunda_columna_esta_se_coloca_en_la_segunda_columna_de_la_ultima_fila_pero_esta_es_distinta_que_la_primera_ficha() {
+        
+        
+        } 
+    }
+}
+    
---
+++ tresenraya.domain/Tablero.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace tresenraya.domain
+{
+   public class Tablero
+    {
+       private string [] _tablero;
+       public Tablero() {
+           _tablero = new [] {"","",""};
+       }
+        public void addFicha(string ficha, int columna)
+        {
+
+            _tablero[columna] = ficha;   
+        }
+
+        public string getCelda(int fila, int columna)
+        {
+            return _tablero[columna];
+        }
+    }
+}
