ade44908be477fb8859956161c43265e6f07ca92
Alex <AlejandroDguez@gmail.com>
2012-06-05 22:52:06 +0200

    Se añaden más test y se han realizado algunas refactorizaciones.

--- tresenraya.domain.test/TableroTest.cs
+++ tresenraya.domain.test/TableroTest.cs
@@ -1,26 +1,87 @@
-﻿
-
+using System;
 using Xunit;
+
 namespace tresenraya.domain.test
 {
     public class TableroTest
     {
-        private string ficha="*";
-        private int columna=0;
-        private int fila=2;
         [Fact]
-        public void cuando_añado_la_primera_ficha_en_la_primera_columna_esta_se_coloca_en_la_ultima_fila_de_esa_columna() 
+        public void si_inicio_el_tablero_este_no_tiene_fichas()
         {
-            var tablero = new Tablero();
-            
-            tablero.addFicha(ficha,columna);
-            Assert.Equal(ficha,tablero.getCelda(fila,columna));
+            Tablero tablero = new Tablero();
+            Assert.Equal(0, tablero.GetNumeroFichas());
         }
+        
         [Fact]
-        public void cuando_añado_una_segunda_ficha_en_la_segunda_columna_esta_se_coloca_en_la_segunda_columna_de_la_ultima_fila_pero_esta_es_distinta_que_la_primera_ficha() {
+        public void si_añado_una_ficha_a_un_tablero_iniciado_este_contendra_una_ficha_en_la_posicion_indicada()
+        {
+            const Fichas ficha = Fichas.Aspa;
+            Tablero tablero = new Tablero();
+            
+            tablero.AddFicha(ficha,0,2);
+           
+            Assert.Equal(1,tablero.GetNumeroFichas());
+            Assert.Equal(ficha,tablero.GetFicha(0,2));
+        }
+
+        [Fact]
+        public void si_existe_una_ficha_en_una_posicion_no_se_puede_poner_otra()
+        {
+            Tablero tablero = new Tablero();
+            tablero.AddFicha(Fichas.Aspa, 0, 0);
+            Assert.Throws<InvalidOperationException>(
+                delegate
+                    {
+                        tablero.AddFicha(Fichas.Circulo, 0, 0);
+                    });
+        }
+
+        [Fact]
+        public void diferencia_entre_los_dos_tipos_de_fichas_sobre_el_tablero_siempre_es_cero_o_uno()
+        {
+            Tablero tablero=new Tablero();
+            tablero.AddFicha(Fichas.Aspa,0,0);
+            Assert.Equal(1,Math.Abs(tablero.GetNumeroFichas(Fichas.Aspa)-tablero.GetNumeroFichas(Fichas.Circulo)));
+            tablero.AddFicha(Fichas.Circulo,0,1);
+            Assert.Equal(0, Math.Abs(tablero.GetNumeroFichas(Fichas.Aspa) - tablero.GetNumeroFichas(Fichas.Circulo)));  
+        }
+
+        [Fact]
+        public void si_todas_las_fichas_de_misma_fila_son_iguales_ganan_esas_fichas()
+        {
+            Tablero tablero = new Tablero();
+
+            tablero.AddFicha(Fichas.Aspa, 0, 0);
+            tablero.AddFicha(Fichas.Aspa, 0, 1);
+            Assert.Equal(null, tablero.GetGanador());
+            tablero.AddFicha(Fichas.Aspa, 0, 2);
+            Assert.Equal(Fichas.Aspa, tablero.GetGanador());
+        }
         
-        
-        } 
+        [Fact]
+        public void si_todas_las_fichas_de_misma_columna_son_iguales_ganan_esas_fichas()
+        {
+            Tablero tablero = new Tablero();
+
+            tablero.AddFicha(Fichas.Aspa, 1, 0);
+            tablero.AddFicha(Fichas.Aspa, 1, 1);
+            Assert.Equal(null, tablero.GetGanador());
+            tablero.AddFicha(Fichas.Aspa, 1, 2);
+            Assert.Equal(Fichas.Aspa, tablero.GetGanador());   
+        }
+
+        [Fact]
+        public void si_todas_las_fichas_de_misma_diagonal_son_iguales_ganan_esas_fichas()
+        {
+            Tablero tablero = new Tablero();
+
+            tablero.AddFicha(Fichas.Aspa, 0, 2);
+            tablero.AddFicha(Fichas.Aspa, 1, 1);
+            Assert.Equal(null, tablero.GetGanador());
+            tablero.AddFicha(Fichas.Aspa, 2, 0);
+            Assert.Equal(Fichas.Aspa, tablero.GetGanador());
+        }
+
+
     }
 }
-    
--- tresenraya.domain/Tablero.cs
+++ tresenraya.domain/Tablero.cs
@@ -1,22 +1,122 @@
-ï»¿
+using System;
 
 namespace tresenraya.domain
 {
    public class Tablero
-    {
-       private string [] _tablero;
-       public Tablero() {
-           _tablero = new [] {"","",""};
+   {
+       private const int size = 3;
+       private readonly Fichas? [,] _tablero =new Fichas?[size,size];
+       public int GetNumeroFichas()
+       {
+           int numFichas = 0;
+           for (int i = 0; i < size; i++)
+               for (int j = 0; j < size; j++)
+                   if (_tablero[i, j] !=null)
+                       numFichas++;
+           return numFichas;
        }
-        public void addFicha(string ficha, int columna)
-        {
 
-            _tablero[columna] = ficha;   
-        }
+       public Fichas? GetFicha(int x, int y)
+       {
+           return _tablero[x,y];
+       }
 
-        public string getCelda(int fila, int columna)
-        {
-            return _tablero[columna];
-        }
-    }
+       public void AddFicha(Fichas ficha, int x, int y)
+       {
+           if (_tablero[x, y] != null)
+               throw new InvalidOperationException("Ya existe una ficha en esa posición.");
+           
+           _tablero[x,y] = ficha;
+       }
+
+       public int GetNumeroFichas(Fichas fichas)
+       {
+           int numFichas = 0;
+           for (int i = 0; i < size; i++)
+               for (int j = 0; j < size; j++)
+                   if (_tablero[i, j] != fichas)
+                       numFichas++;
+           return numFichas;
+       }
+
+       public Fichas? GetGanador()
+       {
+           Fichas? fichaEncontrada = null;
+           int numVeces = 0;
+
+           //Comprobamos por filas
+           for (int i = 0; i < size; i++)
+           {
+               fichaEncontrada = null;
+               numVeces = 0;
+
+               for (int j = 0; j < size; j++)
+               {
+                   //Si hay una casilla vacia, o el tipo de ficha no coincide con la anteriore, saltamos a la siguiente fila a comprobar.
+                   if (_tablero[i, j] == null || fichaEncontrada != null && _tablero[i, j] != fichaEncontrada)
+                       break;
+
+                   numVeces++;
+                   fichaEncontrada = _tablero[i, j];
+               }
+               if (numVeces == size)
+                   return fichaEncontrada;
+           }
+
+           //Comprobamos por columnas
+           for (int j = 0; j < size; j++)
+           {
+               fichaEncontrada = null;
+               numVeces = 0;
+
+               for (int i = 0; i < size; i++)
+               {
+                   //Si hay una casilla vacia, o el tipo de ficha no coincide con la anteriore, saltamos a la siguiente columna a comprobar.
+                   if (_tablero[i, j] == null || fichaEncontrada != null && _tablero[i, j] != fichaEncontrada)
+                       break;
+
+                   numVeces++;
+                   fichaEncontrada = _tablero[i, j];
+               }
+               if (numVeces == size)
+                   return fichaEncontrada;
+           }
+
+           //Primera Diagonal
+           fichaEncontrada = null;
+           numVeces = 0;
+           for (int i = 0; i < size; i++)
+           {  
+                   //Si hay una casilla vacia, o el tipo de ficha no coincide con la anteriore, saltamos a la siguiente fila a comprobar.
+                   if (_tablero[i, i] == null || fichaEncontrada != null && _tablero[i, i] != fichaEncontrada)
+                       break;
+
+                   numVeces++;
+                   fichaEncontrada = _tablero[i, i];
+           }
+
+           if (numVeces == size)
+              return fichaEncontrada;
+           
+           //Segunda Diagonal
+           fichaEncontrada = null;
+           numVeces = 0;
+           for (int i = 0; i < size; i++)
+           {  
+                   //Si hay una casilla vacia, o el tipo de ficha no coincide con la anteriore, saltamos a la siguiente fila a comprobar.
+                   if (_tablero[i, size-1-i] == null || fichaEncontrada != null && _tablero[i, size-1-i] != fichaEncontrada)
+                       break;
+
+                   numVeces++;
+                   fichaEncontrada = _tablero[i, size - 1 - i];
+           }
+
+           if (numVeces == size)
+              return fichaEncontrada;
+           
+           return null;
+       }
+
+   }
+
 }
