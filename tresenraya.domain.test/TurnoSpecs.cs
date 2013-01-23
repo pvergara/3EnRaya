b5f1e573a3841fe0f9fcefa3fb286a1384a466dc
pvergara <pablocristobal@gmail.com>
2013-01-24 00:57:14 +0100

    Agregamos una clase (evidentemente creada desde los test) y sus test correspondientes para empezar con la gestión de los turnos.

---
+++ tresenraya.domain.test/TurnoSpecs.cs
@@ -0,0 +1,46 @@
+﻿using Xunit;
+
+namespace tresenraya.domain.test
+{
+    public class TurnoSpecs
+    {
+        //RED GREEN REFACTOR!!!!!!
+        [Fact]
+        public void si_empiezo_con_un_turno_la_primera_ficha_es_de_tipo_aspa()
+        {
+            var turno = new Turno();
+            Assert.Equal(Fichas.Aspa,turno.GetFichaActual());
+        }
+
+        //RED GREEN REFACTOR!!!!!!
+        [Fact]
+        public void la_segunda_ficha_es_de_tipo_circulo()
+        {
+            //Arrange
+            var turno = new Turno();
+            turno.GetFichaActual();
+
+            //Action
+            var fichaActual = turno.GetFichaActual();
+
+            //Assert
+            Assert.Equal(Fichas.Circulo, fichaActual);
+        }
+
+        //RED GREEN REFACTOR!!!!!!
+        [Fact]
+        public void la_tercera_ficha_es_de_tipo_aspa()
+        {
+            //Arrange
+            var turno = new Turno();
+            turno.GetFichaActual(); //1ª
+            turno.GetFichaActual(); //2ª
+
+            //Action
+            var fichaActual = turno.GetFichaActual(); //3ª
+
+            //Assert
+            Assert.Equal(Fichas.Aspa, fichaActual);
+        }
+    }
+}
---
+++ tresenraya.domain/Turno.cs
@@ -0,0 +1,24 @@
+﻿namespace tresenraya.domain
+{
+    public class Turno
+    {
+        private Fichas _fichaActual;
+
+        public Turno()
+        {
+            _fichaActual = Fichas.Aspa;
+        }
+
+        public Fichas GetFichaActual()
+        {
+            var resultado = _fichaActual;
+
+            if (_fichaActual == Fichas.Aspa)
+                _fichaActual = Fichas.Circulo;
+            else
+                _fichaActual = Fichas.Aspa;
+
+            return resultado;
+        }
+    }
+}
