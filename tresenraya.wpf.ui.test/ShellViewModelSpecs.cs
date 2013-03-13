860076be26d6f02b83f8eabcd203b15f603cc64c
pvergara <pablocristobal@gmail.com>
2013-03-13 23:02:39 +0100

    Paso el proyecto de test de la vista (wpf.ui.test) a NUnit

--- tresenraya.wpf.ui.test/ShellViewModelSpecs.cs
+++ tresenraya.wpf.ui.test/ShellViewModelSpecs.cs
@@ -1,22 +1,23 @@
-﻿using tresenraya.domain;
+﻿using NUnit.Framework;
+using tresenraya.domain;
 using tresenraya.wpf.ui.Views;
-using Xunit;
 
 namespace tresenraya.wpf.ui.test
 {
+    [TestFixture]
     public class ShellViewModelSpecs
     {
-        [Fact]
+        [Test]
         public void init()
         {
             var shellViewModel = new ShellViewModel();
 
-            Assert.Equal(0, shellViewModel.Tablero.GetNumeroFichas());
-            
-            Assert.Equal(Fichas.Aspa, shellViewModel.Turno.GetFichaActual());
+            Assert.AreEqual(0, shellViewModel.Tablero.GetNumeroFichas());
+
+            Assert.AreEqual(Fichas.Aspa, shellViewModel.Turno.GetFichaActual());
         }
 
-        [Fact]
+        [Test]
         public void si_empiezo_a_jugar_y_pulso_sobre_iniciar_partida_esta_vuelve_al_estado_inicial()
         {
             //Arrange
@@ -25,21 +26,31 @@ namespace tresenraya.wpf.ui.test
             var fichaActual = shellViewModel.Turno.GetFichaActual();
             shellViewModel.Tablero.AddFicha(fichaActual, new Posicion(0, 0));
 
-            Assert.NotEqual(0, shellViewModel.Tablero.GetNumeroFichas());
-            Assert.NotEqual(Fichas.Circulo, fichaActual);
+            Assert.AreNotEqual(0, shellViewModel.Tablero.GetNumeroFichas());
+            Assert.AreNotEqual(Fichas.Circulo, fichaActual);
 
             //Action
             shellViewModel.IniciarNuevaPartida();
 
             //Assert
-            Assert.Equal(0, shellViewModel.Tablero.GetNumeroFichas());
-            Assert.Equal(Fichas.Aspa, shellViewModel.Turno.GetFichaActual());
+            Assert.AreEqual(0, shellViewModel.Tablero.GetNumeroFichas());
+            Assert.AreEqual(Fichas.Aspa, shellViewModel.Turno.GetFichaActual());
         }
 
-        [Fact]
+        [Test]
         public void si_añado_una_ficha_en_la_posicion_00_se_situa_en_la_posicion_00_la_ficha_aspa()
         {
-            
+            //Arrange
+            var shellViewModel = new ShellViewModel();
+            var posicion = new Posicion(0, 0);
+            shellViewModel.Tablero.AddFicha(Fichas.Aspa, posicion);
+
+            //Action
+            var ficha = shellViewModel.Tablero.GetFicha(posicion);
+
+            //Assert
+            Assert.AreEqual(Fichas.Aspa, ficha);
+
         }
     }
 }
